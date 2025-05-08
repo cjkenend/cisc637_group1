48271e6aef9db815e680dcedead12bd0d883c71c
cjkenend <116122835+cjkenend@users.noreply.github.com>
2025-05-07 22:26:10 -0400

    Update address_procedure.sql

--- target_code/procedures/address_procedure.sql
+++ target_code/procedures/address_procedure.sql
@@ -1,1 +1,95 @@
 
+-- Goal is to make a procedure that is called gets an address from the table 
+
+/* 
+    Call the procedure with all of the variables for an insert on the table
+    along with the source_table, dest_table, dest_col_name, 
+    and in this case address_num
+*/
+
+CREATE PROCEDURE ud_cisc_group1_target.insert_or_get_address(
+    in_source_table_name        IN VARCHAR2,
+    in_dest_table_name          IN VARCHAR2,
+    in_dest_col_name            IN VARCHAR2,
+    in_address_num              IN INT,
+
+    in_address_value            IN VARCHAR2,
+    in_address_city             IN VARCHAR2,
+    in_address_state            IN VARCHAR2,
+    in_address_zip              IN VARCHAR2,
+
+    in_address_crtd_id          IN VARCHAR2,
+    in_address_crtd_dt          IN VARCHAR2,
+    
+    in_address_updt_id          IN VARCHAR2,
+    in_address_updt_dt          IN VARCHAR2,
+
+    out_address_id              OUT VARCHAR2
+)
+IS
+    v_sql           VARCHAR2(1000);
+    v_table_name    VARCHAR2(5);        -- Might need to change
+
+
+BEGIN
+    --Setting the nocount to check if it exists and declaring variables
+    SET NOCOUNT ON;
+    DECLARE @AddressID VARCHAR(38);
+
+    --Get names 
+    v_addr := 'address' || in_address_num;
+    v_table_name := 'tbl_' || in_source_table_name;
+
+    --Set up check for address
+    v_sql := '
+        SELECT address_id 
+        FROM ' || v_table_name || '
+        WHERE address_value = :address_value
+        AND address_city = :address_city
+        AND address_state = :address_state
+        AND address_zip = :address_zip';
+
+    --Now go through the execution process
+    BEGIN
+        --Execute
+        EXECUTE IMMEDIATE v_sql
+        INTO out_address_id
+        USING in_address_value, in_address_city, in_address_state, in_address_zip;
+    
+    --Set an exception if data isn't found
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            
+            --Handle the insert now 
+            v_sql := '
+                INSERT INTO ud_cisc637_group1_target.' || in_dest_table_name || '(
+                    address_value, address_city, address_state, address_zip, 
+                    address_address_type_id,
+                    address_crtd_id, address_crtd_dt,
+                    address_updt_id, address_updt_dt
+                )
+                VALUES (
+                    :address_value, :address_city, :address_state, :address_zip, 
+                    :address_address_type_id,
+                    :address_crtd_id, :address_crtd_dt,
+                    :address_updt_id, :address_updt_dt
+                )
+                RETURNING address_id INTO :new_address_id';
+            
+            --Execute 
+            EXECUTE IMMEDIATE v_sql
+            USING in_address_value, in_address_city, in_address_state, in_address_zip,
+                in_address_type_id,
+                in_address_crtd_id, in_address_crtd_dt,
+                in_address_updt_id, in_address_updt_dt;
+        
+    END;
+    
+    --Raise if nothing is found or inserted
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE;
+END;
+/
+
+
