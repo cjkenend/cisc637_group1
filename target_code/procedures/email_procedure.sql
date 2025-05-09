bdbc85a8c37d0c640575394e223eb7aac987f3ee
cjkenend <116122835+cjkenend@users.noreply.github.com>
2025-05-09 12:19:26 -0400

    Update email_procedure.sql

--- target_code/procedures/email_procedure.sql
+++ target_code/procedures/email_procedure.sql
@@ -1,24 +1,25 @@
--- Goal is to make a procedure that is called gets an address from the table 
+-- Goal is to make a procedure that is called gets an email from the table 
 
 /* 
     Call the procedure with all of the variables for an insert on the table
-    along with the source_table, dest_table, dest_col_name, 
-    and in this case address_num
+    along with the source_table, dest_table, 
+    and in this case email_num
 */
 
 CREATE OR REPLACE PROCEDURE ud_cisc637_group1_target.insert_or_get_email(
-    IN_EMAIL                 IN VARCHAR2,
+    in_dest_table_name          IN VARCHAR2,
 
-    IN_EMAIL_NAME            IN VARCHAR2,
-    IN_EMAIL_TYPE_ID         IN VARCHAR2,
+    in_email_name             IN VARCHAR2,
 
-    IN_EMAIL_CRTD_ID         IN VARCHAR2,
-    IN_EMAIL_CRTD_DT          IN VARCHAR2,
+    in_email_type_id            IN VARCHAR2,
+
+    in_email_crtd_id            IN VARCHAR2,
+    in_email_crtd_dt            IN VARCHAR2,
     
-    IN_EMAIL_UPDT_ID          IN VARCHAR2,
-    IN_EMAIL_UPDT_DT          IN VARCHAR2,
+    in_email_updt_id            IN VARCHAR2,
+    in_email_updt_dt            IN VARCHAR2,
 
-    OUT_EMAIL_ID              OUT VARCHAR2
+    out_email_id                OUT VARCHAR2
 )
 IS
     v_sql               VARCHAR2(1000);
@@ -26,19 +27,19 @@ IS
 
 BEGIN
 
-    --Set up check for address
+    --Set up check for email
     v_sql := '
-        SELECT address_id 
-        FROM ' || IN_EMAIL || '
-        WHERE EMAIL_NAME';
+        SELECT email_id 
+        FROM ' || in_dest_table_name || '
+        WHERE email_number = :email_number';
 
     --Now go through the execution process
     BEGIN
     
         --Execute
         EXECUTE IMMEDIATE v_sql
-        INTO OUT_EMAIL_ID
-        USING IN_EMAIL_NAME;
+        INTO out_email_id
+        USING in_email_name;
     
     --Set an exception if data isn't found
     EXCEPTION
@@ -46,19 +47,19 @@ BEGIN
             
             --Handle the insert now 
             v_sql := '
-                INSERT INTO ud_cisc637_group1_target.' || IN_EMAIL || '(
-                    EMAIL_NAME, 
-                    EMAIL_EMAIL_TYPE_ID,
-                    EMAIL_CRTD_ID, EMAIL_CRTD_DT,
-                    EMAIL_UPDT_ID, EMAIL_UPDT_ID
+                INSERT INTO ud_cisc637_group1_target.' || in_dest_table_name || '(
+                    email_number, 
+                    email_email_type_id,
+                    email_crtd_id, email_crtd_dt,
+                    email_updt_id, email_updt_dt
                 )
                 VALUES (
-                    :EMAIL_NAME,
-                    :EMAIL_EMAIL_TYPE_ID,
-                    :EMAIL_CRTD_ID, :EMAIL_CRTD_DT,
-                    :ADDRESS_UPDT_ID, :ADDRESS_UPDT_ID
+                    :email_number, 
+                    :email_email_type_id,
+                    :email_crtd_id, :email_crtd_dt,
+                    :email_updt_id, :email_updt_dt
                 )
-                RETURNING EMAIL_ID INTO :NEW_EMAIL_ID';
+                RETURNING email_id INTO :new_email_id';
             
             --Execute 
             EXECUTE IMMEDIATE v_sql
